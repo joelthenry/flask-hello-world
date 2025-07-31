3038ca39fb6387ead6b1170d6c1b2610a1f61e0d
joelthenry <joelthenry@gmail.com>
2025-07-30 23:39:07 -0600

    fixed db url

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-import psycopg
+import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
 
 # your Render Postgres URL
-DATABASE_URL = 'postgresql://ex_database_user:ArMJEGHvOpN87glr90waAZ4IxjJRuaw0@dpg-d25a1sffte5s73c2bje0-a/ex_database'
+DATABASE_URL = (
+    'postgresql://ex_database_user:ArMJEGHvOpN87glr90waAZ4IxjJRuaw0@dpg-d25a1sffte5s73c2bje0-a/ex_database'
+)
 
 @app.route('/')
 def hello_world():
@@ -12,13 +14,13 @@ def hello_world():
 
 @app.route('/db_test')
 def db_test():
-    conn = psycopg.connect(DATABASE_URL, sslmode='require')
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     conn.close()
     return 'Database connection successful'
 
 @app.route('/db_create')
 def db_create():
-    conn = psycopg.connect(DATABASE_URL, sslmode='require')
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cur = conn.cursor()
     cur.execute("""
       CREATE TABLE IF NOT EXISTS Basketball (
@@ -36,7 +38,7 @@ def db_create():
 
 @app.route('/db_insert')
 def db_insert():
-    conn = psycopg.connect(DATABASE_URL, sslmode='require')
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cur = conn.cursor()
     cur.execute("""
       INSERT INTO Basketball (First, Last, City, Name, Number)
@@ -53,7 +55,7 @@ def db_insert():
 
 @app.route('/db_select')
 def db_select():
-    conn = psycopg.connect(DATABASE_URL, sslmode='require')
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cur = conn.cursor()
     cur.execute("SELECT * FROM Basketball;")
     rows = cur.fetchall()
@@ -61,21 +63,4 @@ def db_select():
     conn.close()
 
     html = '<table border="1">'
-    html += '<tr><th>First</th><th>Last</th><th>City</th><th>Name</th><th>Number</th></tr>'
-    for first, last, city, name, number in rows:
-        html += f'<tr><td>{first}</td><td>{last}</td><td>{city}</td><td>{name}</td><td>{number}</td></tr>'
-    html += '</table>'
-    return html
-
-@app.route('/db_drop')
-def db_drop():
-    conn = psycopg.connect(DATABASE_URL, sslmode='require')
-    cur = conn.cursor()
-    cur.execute("DROP TABLE IF EXISTS Basketball;")
-    conn.commit()
-    cur.close()
-    conn.close()
-    return 'Basketball Table Dropped'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    html += '<tr><th>First</th><th>Last</th><th>City</th><th>Na
